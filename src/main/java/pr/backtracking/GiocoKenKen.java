6f897c6bde712a2906f1794064185a126033579e
andrea <andrea.nuara02@gmail.com>
2024-05-23 22:20:46 +0200

    Controllo blocchi funzionante parzialmente

--- src/main/java/pr/backtracking/GiocoKenKen.java
+++ src/main/java/pr/backtracking/GiocoKenKen.java
@@ -140,7 +140,6 @@ public class GiocoKenKen implements Backtracking<Cella, Integer> {
                         }
                         break;
                     default:
-                        // Caso di operazione vuota
                         if (ePieno(b,griglia)&&ris != b.getRisultato()) {
                             return false;
                         }
@@ -200,42 +199,45 @@ public class GiocoKenKen implements Backtracking<Cella, Integer> {
         int dimensione = 3;
         Configurazione configurazione = new Configurazione();
         configurazione.setDimensione(dimensione);
-        Blocco[] blocchi = new Blocco[5];
+
+        Blocco[] blocchi = new Blocco[4];
         Blocco b1 = new Blocco();
         b1.setDimensione(2);
-        b1.setOperazione("*");
-        b1.setRisultato(6);
+        b1.setOperazione("/");
+        b1.setRisultato(3);
         Cella[] celle1=new Cella[2];
-        Cella c1 = new Cella(0,0);
-        Cella c2 = new Cella(1,0);
+        Cella c1 = new Cella(1,2);
+        Cella c2 = new Cella(2,2);
         celle1[0]=c1;
         celle1[1]=c2;
         b1.setCelle(celle1);
         blocchi[0]=b1;
 
         Blocco b2 = new Blocco();
-        b2.setDimensione(2);
-        b2.setOperazione("-");
-        b2.setRisultato(1);
-        Cella[] celle2=new Cella[2];
-        Cella c3 = new Cella(2,1);
-        Cella c4 = new Cella(2,2);
+        b2.setDimensione(3);
+        b2.setOperazione("+");
+        b2.setRisultato(7);
+        Cella[] celle2=new Cella[3];
+        Cella c3 = new Cella(2,0);
+        Cella c4 = new Cella(2,1);
+        Cella c5 = new Cella(1,0);
         celle2[0]=c3;
         celle2[1]=c4;
+        celle2[2]=c5;
         b2.setCelle(celle2);
         blocchi[1]=b2;
 
         Blocco b3 = new Blocco();
         b3.setDimensione(3);
-        b3.setOperazione("+");
-        b3.setRisultato(4);
+        b3.setOperazione("*");
+        b3.setRisultato(3);
         Cella[] celle3=new Cella[3];
-        Cella c5 = new Cella(0,1);
-        Cella c6 = new Cella(1,1);
-        Cella c7 = new Cella(1,2);
-        celle3[0]=c5;
-        celle3[1]=c6;
-        celle3[2]=c7;
+        Cella c6 = new Cella(0,0);
+        Cella c7 = new Cella(0,1);
+        Cella c8 = new Cella(1,1);
+        celle3[0]=c6;
+        celle3[1]=c7;
+        celle3[2]=c8;
         b3.setCelle(celle3);
         blocchi[2]=b3;
 
@@ -243,25 +245,15 @@ public class GiocoKenKen implements Backtracking<Cella, Integer> {
         Blocco b4 = new Blocco();
         b4.setDimensione(1);
         b4.setOperazione("");
-        b4.setRisultato(1);
+        b4.setRisultato(2);
         Cella[] celle4=new Cella[1];
-        Cella c8 = new Cella(2,0);
-        celle4[0]=c8;
+        Cella c9 = new Cella(0,2);
+        celle4[0]=c9;
         b4.setCelle(celle4);
         blocchi[3]=b4;
 
-        Blocco b5 = new Blocco();
-        b5.setDimensione(1);
-        b5.setOperazione("");
-        b5.setRisultato(1);
-        Cella[] celle5=new Cella[1];
-        Cella c9 = new Cella(0,2);
-        celle5[0]=c9;
-        b5.setCelle(celle5);
-        blocchi[4]=b5;
 
-
-        configurazione.setNumeroBlocchi(5);
+        configurazione.setNumeroBlocchi(4);
         configurazione.setBlocchi(blocchi);
 
         int[][] griglia = new int[dimensione][dimensione];
